b34720341498fab2ec23f6460acf2ac1b5d4e853
Alina <123@gmail.com>
2021-12-02 23:56:12 +0500

    make test coverage

--- test.py
+++ test.py
@@ -9,9 +9,9 @@ def test_add_money():
 
 
 data = [
-    ([100, 1000, 100], 1200),
-    ([100, 1000, 100], 1200),
-    ([100, 1000, 100], 1200),
+    ([10, 50, 100], 160),
+    ([100, 3000, 1], 3101),
+    ([210, 140, 100], 450),
 
 ]
 
@@ -26,7 +26,9 @@ def test_add_money_1(balance_list, expected_balance):
 
 
 data_1 = [
-    (2000, [20, 50, 100], 1830)
+    (2000, [20, 50, 100], 1830),
+    (1500, [30, 65, 1000], 405),
+    (1700, [600, 1000, 100], 0),
 ]
 
 
@@ -36,4 +38,18 @@ def test_cross(start_balance, balance, expected_balance):
     account.add_money(start_balance)
     for el in balance:
         account.withdraw_money(el)
+    assert account.get_balance() == expected_balance
+
+
+data_2 = [
+    (1000, 1000),
+    (100, 100),
+    (15, 15)
+]
+
+
+@pytest.mark.parametrize("start_balance, expected_balance", data_2)
+def test_balance(start_balance, expected_balance):
+    account = Account()
+    account.add_money(start_balance)
     assert account.get_balance() == expected_balance
